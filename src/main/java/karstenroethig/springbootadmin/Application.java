d5d28e86db4455343014244ad17f8371aebcf22d
Karsten Röthig <karsten@Karstens-MacBook-Pro.local>
2016-07-09 15:26:18 +0200

    initial commit

---
+++ src/main/java/karstenroethig/springbootadmin/Application.java
@@ -0,0 +1,19 @@
+package karstenroethig.springbootadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAdminServer
+@EnableAutoConfiguration
+public class Application {
+	
+	public static void main( String[] args ) {
+		SpringApplication.run( Application.class, args );
+	}
+}
